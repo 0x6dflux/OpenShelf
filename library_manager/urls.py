c31952f65add501bd7fcf6416826c5f2bb1109ca
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-06-01 22:11:37 +0330

    Add home view
    The home view has been forgotten to be added in the url path.

--- library_manager/urls.py
+++ library_manager/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from library_manager.views import DetailView, ListView
+from library_manager.views import DetailView, HomeView, ListView
 
 app_name = "lib_mngr"
 urlpatterns = [
     path("<str:entity>/detail/<int:identifier>/", DetailView.as_view(), name="detail"),
     path("<str:entity>/list/", ListView.as_view(), name="list"),
-    path('', , name='home')
+    path("", HomeView.as_view(), name="home"),
 ]
